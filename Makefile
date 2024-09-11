ff675fe69ae780aebc7b86066a9eb6fbb09478c4
dmytro7585 <oleksiydima@gmail.com>
2024-09-11 12:47:38 +0300

    bug fix, edit README.md

--- Makefile
+++ Makefile
@@ -1,19 +1,14 @@
-# Компілятор та опції
 CC = gcc
 CFLAGS = -Os -std=c11 -flto -g -Wall -Wextra -Werror
 LDFLAGS = -flto
 
-# Ціль за замовчуванням
 all: watchman
 
-# Компіляція програми
 watchman: watchman.o
 	$(CC) $(LDFLAGS) -o watchman watchman.o
 
-# Компіляція об'єктного файлу
 watchman.o: watchman.c
 	$(CC) $(CFLAGS) -c watchman.c -o watchman.o
 
-# Очищення
 clean:
 	rm -f watchman watchman.o
--- watchman.c
+++ watchman.c
@@ -1,133 +1,118 @@
-#define _XOPEN_SOURCE 700  // Для POSIX функцій. Це робиться для того, щоб увімкнути доступ до розширених POSIX-функцій (зокрема функцій роботи з сигналами, часовими зонами тощо)
-
-#include <stdio.h> // Бібліотека для роботи з введенням/виведенням (функції printf, getchar тощо).
-#include <stdlib.h> // Бібліотека для стандартних функцій, таких як malloc, exit.
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
-#include <unistd.h> // Бібліотека для POSIX-систем (функції роботи з процесами та файлами, такі як sleep, getopt).
-#include <signal.h> // Для роботи з сигналами (функції signal)
-#include <termios.h> // Для роботи з налаштуваннями термінала (зокрема зміна режиму термінала).
-#include <fcntl.h> //Для роботи з файлами (функції, такі як fcntl для налаштування файлових дескрипторів).
-#include <getopt.h> //Для роботи з аргументами командного рядка.
+#include <unistd.h>
+#include <signal.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <getopt.h>
 #include <string.h>
 
-// Структура для збереження опцій програми
-struct program_options 
+static struct watchman_s
 {
-    bool show_seconds;
-    bool show_date;
-    bool show_timezone;
-    bool show_month;
-    bool show_weekday;
-    bool am_pm_format;
-    bool show_utc;
-};
+    time_t raw_time;
+    struct tm *tim_info;
+    struct
+    {
+        bool show_seconds;
+        bool show_date;
+        bool show_timezone;
+        bool show_month;
+        bool show_weekday;
+        bool am_pm_format;
+        bool show_utc;
+    } options;
+    volatile bool running;
+} watchman_s;
 
-// Глобальна змінна для керування циклом виконання програми
-volatile sig_atomic_t running = 1; //volatile: Це ключове слово вказує компілятору, що значення змінної може змінюватися непередбачувано, наприклад, під час обробки сигналу, тому її не можна оптимізувати, Це тип, який гарантує, що операції з цією змінною будуть виконуватися атомарно, тобто без переривань, що важливо при обробці сигналів.
-
-// Оголошення функцій
-static void show_time(const struct tm *tim, const struct program_options *opts);
+static void show_time(const struct tm *tim);
 static void handle_signal(int signal);
-static void set_nonblocking_mode(int enable);
+static void set_nonblocking_mode(bool enable);
 static void print_help(void);
 
 int main(int argc, char *argv[]) 
 {
-    struct program_options options = {false, false, false, false, false, false, false};
+    memset(&watchman_s, 0, sizeof(watchman_s));
+    watchman_s.running = true;
     int opt;
 
-    // Обробка аргументів командного рядка
     while ((opt = getopt(argc, argv, "hsdmtuwa")) != -1) 
     {
-        switch (opt) {
+        switch (opt) 
+        {
             case 'h':
-                print_help();
-                return 0;
-            case 's':
-                options.show_seconds = true;
-                break;
-            case 'd':
-                options.show_date = true;
-                break;
-            case 't':
-                options.show_timezone = true;
-                break;
-            case 'm':
-                options.show_month = true;
-                break;
-            case 'u':
-                options.show_utc = true;
-                break;
-            case 'w':
-                options.show_weekday = true;
-                break;
-            case 'a':
-                options.am_pm_format = true;
-                break;
             default:
                 print_help();
-                return 1;
+                exit(EXIT_FAILURE);
+            case 's':
+                watchman_s.options.show_seconds = true;
+                break;
+            case 'd':
+                watchman_s.options.show_date = true;
+                break;
+            case 't':
+                watchman_s.options.show_timezone = true;
+                break;
+            case 'm':
+                watchman_s.options.show_month = true;
+                break;
+            case 'u':
+                watchman_s.options.show_utc = true;
+                break;
+            case 'w':
+                watchman_s.options.show_weekday = true;
+                break;
+            case 'a':
+                watchman_s.options.am_pm_format = true;
+                break;
         }
     }
 
-    // Налаштування обробників сигналів
     signal(SIGINT, handle_signal);
     signal(SIGTERM, handle_signal);
     signal(SIGQUIT, handle_signal);
 
-    // Увімкнення неблокуючого режиму вводу
-    set_nonblocking_mode(1);
+    set_nonblocking_mode(true);
 
-    // Основний цикл для оновлення часу
-    while (running) 
+    while (watchman_s.running) 
     {
-        time_t raw_time;
-        struct tm *tim_info;
-
-        // Отримання поточного часу
-        time(&raw_time);
-        if (options.show_utc) {
-            tim_info = gmtime(&raw_time);
-        } else {
-            tim_info = localtime(&raw_time);
+        time(&watchman_s.raw_time);
+        if (watchman_s.options.show_utc) 
+        {
+            watchman_s.tim_info = gmtime(&watchman_s.raw_time);
+        } 
+        else 
+        {
+            watchman_s.tim_info = localtime(&watchman_s.raw_time);
         }
 
-        // Вивід часу
-        show_time(tim_info, &options);
+        show_time(watchman_s.tim_info);
 
-        // Перевірка на натискання клавіші 'q' для виходу
         char ch = getchar();
         if (ch == 'q') 
         {
             break;
         }
-
-        // Оновлення часу щосекунди
+        
         sleep(1);
     }
 
-    // Повернення терміналу до звичайного режиму і очищення екрану
-    set_nonblocking_mode(0);
-    printf("\033[2J\033[H");  // Це спеціальні керуючі послідовності ANSI для очищення екрану і переміщення курсора у верхній лівий кут. Так після завершення програми на екрані не залишаться сліди від попереднього виведення.
-    return 0;
+    set_nonblocking_mode(false);
+    printf("\r\033[K");
+    exit(EXIT_SUCCESS);
 }
 
-// Функція для відображення часу
-static void show_time(const struct tm *tim, const struct program_options *opts) 
+static void show_time(const struct tm *tim) 
 {
-    // Очищення попереднього виводу
     printf("\r");
 
-    // Вибір формату часу
-    if (opts->am_pm_format) 
+    if (watchman_s.options.am_pm_format) 
     {
-        // 12-годинний формат з AM/PM
         int hour = tim->tm_hour % 12;
+        if (hour == 0) hour = 12;
 
-        if (hour == 0) hour = 12;  // 12 години в AM/PM форматі
-
-        if (opts->show_seconds) 
+        if (watchman_s.options.show_seconds) 
         {
             printf("[%02d:%02d:%02d", hour, tim->tm_min, tim->tm_sec);
         } 
@@ -138,9 +123,8 @@ static void show_time(const struct tm *tim, const struct program_options *opts)
         printf(tim->tm_hour >= 12 ? " PM]" : " AM]");
     } 
     else 
-    {        
-        // 24-годинний формат
-        if (opts->show_seconds) 
+    {
+        if (watchman_s.options.show_seconds) 
         {
             printf("[%02d:%02d:%02d]", tim->tm_hour, tim->tm_min, tim->tm_sec);
         } 
@@ -150,62 +134,54 @@ static void show_time(const struct tm *tim, const struct program_options *opts)
         }
     }
 
-    // Виведення дати, місяця, дня тижня та часового поясу
-    if (opts->show_weekday) 
+    if (watchman_s.options.show_weekday) 
     {
-        char *weekday[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        static const char *const weekday[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
         printf("[%s]", weekday[tim->tm_wday]);
     }
 
-    if (opts->show_month) 
+    if (watchman_s.options.show_month) 
     {
-        char *month[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        static const char *const month[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
         printf("[%s]", month[tim->tm_mon]);
     }
 
-    if (opts->show_date) 
+    if (watchman_s.options.show_date) 
     {
         printf("[%04d-%02d-%02d]", tim->tm_year + 1900, tim->tm_mon + 1, tim->tm_mday);
     }
 
-    if (opts->show_timezone)
+    if (watchman_s.options.show_timezone) 
     {
         extern char *tzname[2];
-        char *timezone = tzname[tim->tm_isdst > 0];
+        char * timezone = watchman_s.options.show_utc ? "UTC" : tzname[tim->tm_isdst > 0];
+
         printf("[%s]", timezone);
     }
 
-    // Оновлення буферу виводу
-    fflush(stdout); // Це примусове очищення буфера виводу, щоб всі дані були негайно виведені на екран. Без цього виклику виведення могло б затриматися до того моменту, коли буфер заповниться.
+    fflush(stdout);
 }
 
-// Функція обробки сигналів
 static void handle_signal(int signal) 
 {
     if (signal == SIGINT || signal == SIGTERM || signal == SIGQUIT) 
     {
-        running = 0;  // Завершення циклу
+        watchman_s.running = false;
     }
 }
 
-// Функція для встановлення неблокуючого режиму для stdin
-static void set_nonblocking_mode(int enable) // локуючий режим означає, що програма чекає, поки користувач введе щось, а неблокуючий дозволяє програмі продовжувати виконання, навіть якщо нічого не було введено.
+static void set_nonblocking_mode(bool enable) 
 {
     struct termios tattr;
     tcgetattr(STDIN_FILENO, &tattr);
 
     if (enable) 
     {
-        // Увімкнути неблокуючий режим
-        tattr.c_lflag &= ~(ICANON | ECHO);  // Цей рядок вимикає дві функції терміналу:
-        // ICANON: Вимикає канонічний режим. У канонічному режимі введення обробляється по рядках (програма чекає, поки користувач натисне Enter). Вимикання цього режиму дозволяє обробляти введення символ за символом.
-        // ECHO: Вимикає ехо, тобто символи, які вводяться користувачем, більше не будуть відображатися в терміналі.
-        fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK); // Використовується функція fcntl, щоб додати флаг O_NONBLOCK до дескриптора файлу stdin. Це означає, що тепер введення не буде блокувати виконання програми, навіть якщо користувач нічого не ввів.
-    }
-
+        tattr.c_lflag &= ~(ICANON | ECHO);
+        fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK);
+    } 
     else 
     {
-        // Відновити стандартний режим
         tattr.c_lflag |= (ICANON | ECHO);
         fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) & ~O_NONBLOCK);
     }
@@ -213,17 +189,19 @@ static void set_nonblocking_mode(int enable) // локуючий режим оз
     tcsetattr(STDIN_FILENO, TCSANOW, &tattr);
 }
 
-// Функція для виводу допомоги
 static void print_help(void) 
 {
-    printf("Usage: time_display [options]\n");
-    printf("Options:\n");
-    printf("  -h          Show this help message\n");
-    printf("  -s          Show seconds\n");
-    printf("  -d          Show date (year-month-day)\n");
-    printf("  -t          Show timezone\n");
-    printf("  -m          Show month name\n");
-    printf("  -u          Show time in UTC\n");
-    printf("  -w          Show weekday name\n");
-    printf("  -a          Use AM/PM format\n");
+    printf
+    (
+        "Usage: ./watchman [-hsdtmuwa]\n"
+        "Options:\n"
+        "  -h          Show this help message\n"
+        "  -s          Show seconds\n"
+        "  -d          Show date (year-month-day)\n"
+        "  -t          Show timezone\n"
+        "  -m          Show month name\n"
+        "  -u          Show time in UTC\n"
+        "  -w          Show weekday name\n"
+        "  -a          Use AM/PM format\n"
+    );
 }
